5f3a3d34b98c9f05beee226343787bdf49a1c3b1
Hun Kim <hun.kim@nsuslab.com>
2022-05-20 18:31:45 +0900

    pwa test

--- pwa/service-worker.js
+++ pwa/service-worker.js
@@ -47,3 +47,25 @@ self.addEventListener('fetch', e => {
         }
     })());
 });
+
+//notification
+self.addEventListener('notificationclick', (event) => {
+    let url = 'https://aloveu.github.io/pwa/index.html';
+    event.notification.close(); // Android needs explicit close.
+    event.waitUntil(
+        clients.matchAll({type: 'window'}).then( windowClients => {
+            // Check if there is already a window/tab open with the target URL
+            for (var i = 0; i < windowClients.length; i++) {
+                var client = windowClients[i];
+                // If so, just focus it.
+                if (client.url === url && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            // If not, then open the target URL in a new window/tab.
+            if (clients.openWindow) {
+                return clients.openWindow(url);
+            }
+        })
+    );
+});
